4a117de4902d8dbb1078863e2ffa168bc29e200c
unknown <malenahuuy@any.com>
2020-03-08 19:11:57 -0700

    commit

--- app.js
+++ app.js
@@ -24,15 +24,18 @@ if (stepNum ===0 || isNaN(stepNum)) {
     for (let i = startNum; i <= endNum; i += stepNum){
         outputNum.push(i);
     };
+    
     var sum = array.reduceRight(function(a,b){
         return a+b;
     },0);
+    
     var toBinary =  function (decNum){
         return parseInt(decNum,10).toString(2);
     };
     for (let decNum of outputNum) {
     outputDecNum.push(toBinary(decNum));
     };    
+    
     output1.innerHTML='<p>'+`The generated array is ${outputNum.join(',')}`+'</p>';
     output2.innerHTML='<p>'+`The sum is ${sum}`+'<p>';
     output3.innerHTML='<p>'+`The binary of abolut element values are: ${outputDecNum.join(',')}`+'</p>';
@@ -40,15 +43,18 @@ if (stepNum ===0 || isNaN(stepNum)) {
     for (let i = startNum; i >= endNum; i += stepNum){
         outputNum.push(i);
     };
+   
     var sum = array.reduceRight(function(a,b){
         return a+b;
     },0);
+    
     var toBinary =  function (decNum){
         return parseInt(decNum,10).toString(2);
     };
     for (let decNum of outputNum) {
     outputDecNum.push(toBinary(decNum));
     };    
+    
     output1.innerHTML='<p>'+`The generated array is ${outputNum.join(',')}`+'</p>';
     output2.innerHTML='<p>'+`The sum is ${sum}`+'<p>';
     output3.innerHTML='<p>'+`The binary of abolut element values are: ${outputDecNum.join(',')}`+'</p>';
